408835438492f9ee32040c225f5e44c19614ee23
Laiba <laibataj1306@gmail.com>
2026-09-15 12:51:21 +0530

    Updated backend

--- backend/server.js
+++ backend/server.js
@@ -4,17 +4,18 @@ import bcrypt from "bcryptjs";
 import nodemailer from "nodemailer";
 import pool from "./db.js";
 import dotenv from "dotenv";
-dotenv.config();
 
-app.get("/", (req, res) => {
-  res.send("Shnoor Parking Backend is running");
-});
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Shnoor Parking Backend is running");
+});
 
 const initDbSchema = async () => {
   try {
@@ -2401,7 +2402,8 @@ app.post("/api/support-tickets/:id/reply", async (req, res) => {
   }
 });
 
-app.listen(port, () => {
+const PORT = process.env.PORT || 5000;
 
-  console.log(`Server running on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
